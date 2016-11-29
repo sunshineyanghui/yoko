34d4e77b2487c2e7d29225db3af40b293b893f34
sunshineyanghui <1187073986@qq.com>
2016-11-29 11:51:50 +0800

    layout

---
+++ src/component/NavFooter.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+class NavFooter extends React.Component {
+  render () {
+    return(
+      <div>
+        我是底部内容
+      </div>
+    )
+  }
+}
+
+export default NavFooter;
---
+++ src/component/NavHeader.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+class NavHeader extends React.Component {
+  render () {
+    return(
+      <div>
+        我是头部内容
+
+      </div>
+    )
+  }
+}
+
+export default NavHeader;
